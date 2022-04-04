fd17658fb8ba006a5095761771fdef80c4d44521
antti <anta@metropolia.fi>
2022-04-05 02:19:37 +0300

    Sarja 9

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -1,17 +1,47 @@
-
-var request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 request.open('GET', 'data/books.json', false);
 request.send(null);
-var data = JSON.parse(request.responseText);
+const data = JSON.parse(request.responseText);
 console.log(data);
 
-var books = data.books;
+const books = data.books;
 
-var list = document.createElement('ol');
-for (var i=0; i < books.length; i++) {
-	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
-	list.appendChild(item);
+const table = document.createElement('table');
+
+makeItem(null);
+
+for (let x in books) {
+	makeItem(x);
 }
-document.body.appendChild(list);
+
+function makeItem(x){
+
+	let bookTitle = 'Title';
+	let bookYear = 'Year';
+
+	if(x != null){
+		bookTitle = books[x].title;
+		bookYear = books[x].year;
+	}
+
+	console.log(bookTitle);
+	const tableRow = document.createElement('tr');
+
+	const title = document.createElement('th');
+	title.innerHTML = bookTitle;
+	const year = document.createElement('th');
+	year.innerHTML = bookYear;
+
+	tableRow.addEventListener('click', function () {
+		if(x != null){
+			document.getElementById('title').textContent = bookTitle;
+		}
+	})
+
+	tableRow.appendChild(title);
+	tableRow.appendChild(year);
+
+	table.appendChild(tableRow);
+}
+
+document.body.appendChild(table);
--- labs/09 Single-Page Applications/js/lecture_notes.js
+++ labs/09 Single-Page Applications/js/lecture_notes.js
@@ -1,18 +1,28 @@
-var notes = [];
+const notes = [];
 
 /*
  * displays the 'add' screen if this has been bookmarked by user
  */
-if (window.location.hash == '#add' || notes.length === 0) {
+if (window.location.hash === '#add' || notes.length === 0) {
 	document.getElementById('editPage').style.display = 'none';
 } else {
 	document.getElementById('addPage').style.display = 'none';
 }
 
+document.querySelector('#editPage input').addEventListener('input', updateNote);
+document.querySelector('#editPage textarea').addEventListener('input', updateNote);
+
 document.querySelector('#addPage button').onclick = function() {
 	console.log('add note');
-	var title = document.querySelector('#addPage input').value;
-	var note = document.querySelector('#addPage textarea').value;
+	const title = document.querySelector('#addPage input').value;
+	const note = document.querySelector('#addPage textarea').value;
+	let pushNote = {
+		title: title,
+		note: note
+	}
+	notes.push(pushNote);
+	document.querySelector('#addPage input').value = " ";
+	document.querySelector('#addPage textarea').value = " ";
 };
 
 /*
@@ -20,29 +30,34 @@ document.querySelector('#addPage button').onclick = function() {
  */ 
 document.querySelector('nav > ul > li:nth-child(1)').onclick = function() {
 	console.log('first link clicked');
+	document.getElementById('editPage').style.display = 'none';
+	document.getElementById('addPage').style.display = 'block';
 };
 
 document.querySelector('nav > ul > li:nth-child(2)').onclick = function() {
 	console.log('second link clicked');
+	loadList();
+	document.getElementById('addPage').style.display = 'none';
+	document.getElementById('editPage').style.display = 'block';
 };
-
 
 function updateNote() {
 	console.log('update note');
-	var title = document.querySelector('#editPage input').value;
-	var note = document.querySelector('#editPage textarea').value;
-	var id = parseInt(document.querySelector('#editPage p').innerHTML, 10);
+	const title = document.querySelector('#editPage input').value;
+	const note = document.querySelector('#editPage textarea').value;
+	const id = parseInt(document.querySelector('#editPage p').innerHTML, 10);
 	console.log(id);
-	var updated = {title: title, note: note};
+	const updated = {title: title, note: note};
 	console.log(updated);
 	notes[id] = {title: title, note: note};
+	loadList();
 }
 
 function display(element) {
 	console.log('display');
 	console.log(element.parentNode.parentNode.id);
-	var details = document.getElementById('details');
-	var id = element.parentNode.parentNode.id;
+	const details = document.getElementById('details');
+	const id = element.parentNode.parentNode.id;
 	document.querySelector('#editPage input').value = notes[id].title;
 	document.querySelector('#editPage textarea').value = notes[id].note;
 	document.querySelector('#editPage p').innerHTML = id;
@@ -50,14 +65,14 @@ function display(element) {
 
 function rem(element) {
 	console.log('remove');
-	var id = element.parentNode.parentNode.id;
+	const id = element.parentNode.parentNode.id;
 	console.log(id);
 	notes.splice(id, 1);
 	loadList();
-	var editId = parseInt(document.querySelector('#editPage p').innerHTML, 10);
+	const editId = parseInt(document.querySelector('#editPage p').innerHTML, 10);
 	console.log('id: '+id);
 	console.log('editId: '+editId);
-	if (id == editId) {
+	if (id === editId) {
 		console.log('deleted document being edited!');
 		document.querySelector('#editPage input').value = '';
 		document.querySelector('#editPage textarea').value = '';
@@ -65,10 +80,10 @@ function rem(element) {
 }
 
 function loadList() {
-	var table = document.getElementById('list');
+	const table = document.getElementById('list');
 	table.innerHTML = '';
-	for (var i=0; i<notes.length; i++) {
-		var row = document.createElement('tr');
+	for (let i=0; i<notes.length; i++) {
+		const row = document.createElement('tr');
 		row.id = i;
 		row.innerHTML = '<td><a onclick="display(this)" href="#">'+notes[i].title+'</a></td><td><a onclick="rem(this)" class="delete" href="#">delete</a></td>';
 		table.appendChild(row);
